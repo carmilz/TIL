6f140b7f35149a9626f6409950ee2e4a3ae7b66c
CarMilZ <carmilz@outlook.com>
2020-10-17 12:22:23 +0900

    Update code.cpp

--- code.cpp
+++ code.cpp
@@ -1,39 +1,44 @@
+//p449-Part04-Chapter11-InheritAssignOperation
 #include <iostream>
 
-class Point
+class First
 {
 private:
-	int xpos, ypos;
+	int num1, num2;
 public:
-	Point(int x = 0, int y = 0) : xpos(x), ypos(y) { }
-	void ShowPosition() const { std::cout << '[' << xpos << ", " << ypos << ']' << std::endl; }
-	friend std::ostream& operator<<(std::ostream&, const Point&);
-	friend std::istream& operator>>(std::istream&, Point&);
+	First(int n1=0, int n2=0) : num1(n1), num2(n2) { }
+	void ShowData() { std::cout << num1 << ", " << num2 << std::endl; }
+	First& operator=(const First& ref)
+	{
+		std::cout << "First& operator=()" << std::endl;
+		num1 = ref.num1;
+		num2 = ref.num2;
+		return *this;
+	}
 };
 
-std::ostream& operator<<(std::ostream& os, const Point& pos)
+class Second : public First
 {
-	os << '[' << pos.xpos << ", " << pos.ypos << ']' << std::endl;
-	return os;
-}
-
-std::istream& operator>>(std::istream& is, Point& pos)
-{
-	is >> pos.xpos >> pos.ypos;
-	return is;
-}
+private:
+	int num3, num4;
+public:
+	Second(int n1, int n2, int n3, int n4) : First(n1, n2), num3(n3), num4(n4) { }
+	void ShowData() { First::ShowData(); std::cout << num3 << ", " << num4 << std::endl; }
+	Second& operator=(const Second& ref)
+	{
+		std::cout << "Second& operator=()" << std::endl;
+		num3 = ref.num3;
+		num4 = ref.num4;
+		return *this;
+	}
+};
 
 int main()
 {
-	Point pos1;
-	std::cout << "x, y 좌표순으로 입력: ";
-	std::cin >> pos1;
-	std::cout << pos1;
-
-	Point pos2;
-	std::cout << "x, y 좌표순으로 입력: ";
-	std::cin >> pos2;
-	std::cout << pos2;
+	Second ssrc(111, 222, 333, 444);
+	Second scpy(0, 0, 0, 0);
+	scpy = ssrc;
+	scpy.ShowData();
 
 	return 0;
 }
